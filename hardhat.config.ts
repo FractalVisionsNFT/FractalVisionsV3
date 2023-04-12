aed9d480031249d71a6ab340791eec0bf0cd9722
Ultra-tech-code <hizick27@gmail.com>
2023-04-12 09:28:09 +0100

    configured hardhat for testnet for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_mumbai_API_KEY_URL = process.env.ALCHEMY_mumbai_API_KEY_URL;
+const ALCHEMY_OPtimism_API_KEY_URL = process.env.ALCHEMY_OPtimism_API_KEY_URL
+//contract address key
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -10,7 +17,17 @@ const config: HardhatUserConfig = {
         runs: 300,
       },
     },
-  } 
+  },
+  networks: {
+    optimism: {
+      url: ALCHEMY_OPtimism_API_KEY_URL,
+      //@ts-ignore
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
 
 export default config;
